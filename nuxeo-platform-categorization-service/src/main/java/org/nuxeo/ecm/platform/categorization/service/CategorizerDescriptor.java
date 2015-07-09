bbf2befccfe633cdc23cec3d2f23dbd212da010f
Florent Guillaume <fg@nuxeo.com>
2015-07-09 17:36:01 +0200

    NXP-17449: move some exceptions

--- nuxeo-platform-categorization-service/src/main/java/org/nuxeo/ecm/platform/categorization/service/CategorizerDescriptor.java
+++ nuxeo-platform-categorization-service/src/main/java/org/nuxeo/ecm/platform/categorization/service/CategorizerDescriptor.java
@@ -32,8 +32,8 @@ import org.nuxeo.common.xmap.annotation.XNodeMap;
 import org.nuxeo.common.xmap.annotation.XObject;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.ecm.core.api.PropertyException;
 import org.nuxeo.ecm.core.api.model.Property;
-import org.nuxeo.ecm.core.api.model.PropertyException;
 import org.nuxeo.runtime.model.RuntimeContext;
 
 @XObject("categorizer")
